a70df436df35c5c04a3eaaa55f2d1543fd6f0e11
Snakepit7 <julienfollet71@gmail.com>
2018-11-16 11:22:24 +0100

    exos

---
+++ exo1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo1</title>
+  </head>
+  <body>
+    <?php
+        $number = 0;
+        
+        while ($number<=10) {
+            echo $number.' ';
+            $number++;
+        }
+    ?>
+  </body>
+</html>
---
+++ exo2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo2</title>
+  </head>
+  <body>
+      <?php
+        $firstnumber = 0;
+        $secondnumber = 80;
+        
+        while ($firstnumber < 20) {
+            echo $firstnumber*$secondnumber.'<br />';
+            $firstnumber++;
+        }
+      ?>
+  </body>
+</html>
